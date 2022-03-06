79ea1ba01ada46a76a4057ec8891151ed168d4fd
raef <raef.oueslati1996@gmail.com>
2022-03-06 21:10:44 +0100

    Add Encryption type model

---
+++ Cryptography/Models/EncryptionType.cs
@@ -0,0 +1,28 @@
+﻿namespace Cryptography.Models
+{
+    internal class EncryptionType
+    {
+        public const string AsymetricEncryption = "Asymetric Encryption";
+        public const string SymetricEncryption = "Symetric Encryption";
+
+        public enum EncryptionOption
+        {
+            AsymetricEncryption,
+            SymetricEncription,
+            None
+        }
+        
+        public static EncryptionOption FromLabelToOption(string label)
+        {
+            switch (label)
+            {
+                case AsymetricEncryption:
+                    return EncryptionOption.AsymetricEncryption;
+                case SymetricEncryption:
+                    return EncryptionOption.SymetricEncription;
+                default:
+                    return EncryptionOption.None;
+            }
+        }
+    }
+}
