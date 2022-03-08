d328887da7f74fd467b73a5ee6ca831a06fd4d00
raef <raef.oueslati1996@gmail.com>
2022-03-09 00:03:04 +0100

    Add Encryption Service

---
+++ Cryptography.Core/Cryptography.Core/Services/EncryptionService.cs
@@ -0,0 +1,105 @@
+﻿using System.Security.Cryptography;
+using Cryptography.Core.Models;
+
+namespace Cryptography.Core.Services
+{
+    internal class EncryptionService
+    {
+        public static SymmetricKeyAES CreateSymmetricKey()
+        {
+            var aes = Aes.Create();
+
+            aes.GenerateIV();
+            aes.GenerateKey();
+
+            return new SymmetricKeyAES
+            {
+                Key = Convert.ToBase64String(aes.Key),
+                IV = Convert.ToBase64String(aes.IV)
+            };
+        }
+
+        public static AsymmetricKeyRSA CreateAsymmetricKey()
+        {
+            using var rsa = new RSACryptoServiceProvider(2048);
+            try
+            {
+                return new AsymmetricKeyRSA
+                {
+                    PublicKey = rsa.ToXmlString(false),
+                    PrivateKey = rsa.ToXmlString(true)
+                };
+            }
+            finally
+            {
+                rsa.PersistKeyInCsp = false; // keys should not be persisted on the computer
+            }
+        }
+
+        public static string EncryptAES(string plainText, SymmetricKeyAES symmetricKeyAES)
+        {
+            if (string.IsNullOrEmpty(plainText))
+                throw new ArgumentNullException(nameof(plainText));
+
+            if (symmetricKeyAES.IV is null || symmetricKeyAES.Key is null)
+                throw new ArgumentNullException(nameof(symmetricKeyAES));
+
+            byte[] encrypted;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Convert.FromBase64String(symmetricKeyAES.Key);
+                aesAlg.IV = Convert.FromBase64String(symmetricKeyAES.IV);
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using MemoryStream msEncrypt = new();
+                using CryptoStream csEncrypt = new(msEncrypt, encryptor, CryptoStreamMode.Write);
+                using (StreamWriter swEncrypt = new(csEncrypt))
+                {
+                    //Write all data to the stream.
+                    swEncrypt.Write(plainText);
+                }
+
+                encrypted = msEncrypt.ToArray();
+            }
+
+            return Convert.ToBase64String(encrypted);
+        }
+
+        public static string DecryptAES(string encryptedText, SymmetricKeyAES symmetricKeyAES)
+        {
+            if (string.IsNullOrEmpty(encryptedText))
+                throw new ArgumentNullException(nameof(encryptedText));
+
+            if (symmetricKeyAES.IV is null || symmetricKeyAES.Key is null)
+                throw new ArgumentNullException(nameof(symmetricKeyAES));
+
+            byte[] cipherText = Convert.FromBase64String(encryptedText);
+
+            string decryptedText;
+
+            // Create an Aes object with the specified symmetric key.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Convert.FromBase64String(symmetricKeyAES.Key);
+                aesAlg.IV = Convert.FromBase64String(symmetricKeyAES.IV);
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using MemoryStream msDecrypt = new(cipherText);
+                using CryptoStream csDecrypt = new(msDecrypt, decryptor, CryptoStreamMode.Read);
+                using StreamReader srDecrypt = new(csDecrypt);
+                // Read the decrypted bytes from the decrypting stream
+                // and place them in a string.
+                decryptedText = srDecrypt.ReadToEnd();
+            }
+
+            return decryptedText;
+        }
+    }
+}
