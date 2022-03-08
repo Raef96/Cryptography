8f8e4b6866bb3b3282ae2606e2bf85ee6cf6ec5a
raef <raef.oueslati1996@gmail.com>
2022-03-09 00:01:54 +0100

    Add new project core for encryption services

---
+++ Cryptography.Core/Cryptography.Core/Models/AsymmetricKeyRSA.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Cryptography.Core.Models
+{
+    internal class AsymmetricKeyRSA
+    {
+        public string? PublicKey { get; set; }
+        public string? PrivateKey { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
---
+++ Cryptography.Core/Cryptography.Core/Models/SymmetricKeyAES.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Cryptography.Core.Models
+{
+    internal class SymmetricKeyAES
+    {
+        public string? Key { get; set; }
+        public string? IV { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
